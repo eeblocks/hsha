b5e55b4b0c9b291162b6fed1f8d532f04ca425b4
Esiquiel <41625343+esiquiel@users.noreply.github.com>
2021-08-29 12:47:34 +0200

    hsha v1.2

--- hsha.py
+++ hsha.py
@@ -1,39 +1,78 @@
 #!/usr/bin/python3
-# hsha v1.0
+# hsha v1.2
 
 # Dependencies
 from hashlib import sha256
 from sys import exit
+from time import sleep, time
 
 import os
 import getpass
-
+import argparse
 
 # Colors
 class Colors:
     WHITE = "\033[37m"
     RED = "\033[31m"
-    MAGENTA = "\033[32m"
+    GREEN = "\033[32m"
 
 
 # Welcome
 def welcome():
     os.system('cls' if os.name == 'nt' else 'clear')
 
-    banner = open('banner.txt', 'r').read()
-    print(f'{Colors.MAGENTA}{banner}{Colors.WHITE}\n')
+    banner = open('banner.txt', 'r', encoding='utf-8').read()
+    print(f'{Colors.GREEN}{banner}{Colors.WHITE}\n')
+
+
+welcome()
+
+
+# Argument Settings
+parser = argparse.ArgumentParser(description='Hash your Files/Text to SHA-256', add_help=True)
+parser.add_argument('-t', '--text', action='store', required=False, dest='text', help='Text to SHA-256')
+parser.add_argument('-f', '--file', action='store', required=False, dest='file', help='File to SHA-256')
+args = parser.parse_args()
 
 
 # Hash Function
 def hashing(s):
+
     welcome()
 
-    print(sha256(s.encode()).hexdigest())
+    start_time = time()
+    encoded = sha256(s.encode()).hexdigest()
+    end_time = time()
 
+    elapsed = end_time - start_time
+
+    print(encoded)
+    print(f'\nTime elapsed: {Colors.GREEN}{round(elapsed, 2)}s{Colors.WHITE}')
+    
+
+# Text Function
+def text(t):
+    hashing(t)
+
+def file(f):
+
+    try:
+        file_open = open(f, 'r', encoding='cp850').read()
+        hashing(file_open)
+    except:
+        print(f'{Colors.RED}Needs to specify the exact path...{Colors.WHITE}')
+        exit()
 
 # Main
 if __name__ == '__main__':
-    welcome()
+
+    if args.text:
+        text(args.text)
+        exit()
+
+    elif args.file:
+        file(args.file)
+        exit()
 
     print('-------------')
     print('$ Hash Mode $')
@@ -51,13 +90,20 @@ if __name__ == '__main__':
 
         print(f'Current directory:\n{d}')
 
+        print('\n(help*)')
         while True:
+            
             it = input(f'\n[+] File route: ').lower()
 
             if it == 'help*' or it == 'h*':
+                welcome()
                 print(' \ndownloads* / d* > Goes to the download directory')
 
             elif it == 'downloads*' or it == 'd*':
+                welcome()
+
+                print(f'Current directory:\nC:/Users/{getpass.getuser()}/Downloads/\n')
+                
                 down = input('[+] Download name: ')
                 i = f'C:/Users/{getpass.getuser()}/Downloads/{down}'
                 break
@@ -82,4 +128,3 @@ if __name__ == '__main__':
         exit()
 
     hashing(s)
-    
